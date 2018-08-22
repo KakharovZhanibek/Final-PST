d3ff93ed951c8c41eb73df2e924459a874519246
Омаров Кайсар <@local.itstep>
2018-08-22 10:19:28 +0600

    Добавьте файлы проекта.

---
+++ PST.LIB/Modul/StudentWithAdvisor.cs
@@ -0,0 +1,22 @@
+﻿using P_S_T.Modul;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PST.LIB.Modul
+{
+    public class StudentWithAdvisor : Student
+    {
+        public List<Teacher> DB;
+        public Teacher Teacher;
+
+        public static StudentWithAdvisor Random_Student(ref List<StudentWithAdvisor> r_student)
+        {
+            Random rnd = new Random();
+            return r_student[rnd.Next(1, r_student.Count)];
+        }
+    }
+    
+}
---
+++ PST.LIB/Modul/TeacherWithStudent.cs
@@ -0,0 +1,26 @@
+﻿using P_S_T.Modul;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PST.LIB.Modul
+{
+    public class TeacherWithStudent : Teacher
+    {
+        public List<Student> DB;
+        public List<Student> students;
+
+        public TeacherWithStudent()
+        {
+            DB = new List<Student>();
+            students = new List<Student>();
+        }
+        public static TeacherWithStudent Random_Teacher(ref List<TeacherWithStudent>r_teacher)
+        {
+            Random rnd = new Random();
+            return r_teacher[rnd.Next(1, r_teacher.Count)];
+        }
+    }
+}
